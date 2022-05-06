c69bd0a82118f05f37adbd142f034091d652d79b
Onnesok <ratulhasan9464@gmail.com>
2022-05-06 21:46:35 +0600

    init

--- string_fail.py
+++ string_fail.py
@@ -1,19 +1,35 @@
+fs = []
+ls = []
+rs = 0
+rs2 = 0
+
+def exx(x):
+    count = 0
+    while x != 0:
+        if  count == 0:
+            temp = x%10
+            ls.append(temp)
+            x = x//10
+            count = 1
+        elif count == 1:
+            while x != 0:
+                if x//10 == 0:
+                    fs.append(x)
+                x=x//10
+
 user = int(input("please enter an integer: "))
-
-last = []
-first = [] 
-temp = 999999
-
 if user <= 1:
     print("invalid")
-
 else:
     for i in range(user):
-        x = int(input("please enter your ex: "))
-        if temp != 9999999:
-            temp = x % 10
-            first.append(temp)
-        user = x // 10
-        if x % 10 == 0:
-            last.append(user)
+        x = int(input("enter how many ex you had: "))
+        exx(x)
 
+for k in range(len(fs)):
+    rs = rs + fs[k]
+for l in range(len(ls)):
+    rs2 = rs2 + ls[l]
+print(rs)
+print(rs2)
+
+print(fs, ls)
